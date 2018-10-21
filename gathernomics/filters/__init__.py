71ee3cc6cd993d8749b1a95618715828ef39b89f
SuperOxigen <alex@snowycloud.com>
2018-10-21 09:19:43 -0400

    Created Import/Export filter.

--- gathernomics/filters/__init__.py
+++ gathernomics/filters/__init__.py
@@ -8,3 +8,4 @@ from gathernomics.filters.consumption import ConsumptionFilter
 from gathernomics.filters.gdp import GDPFilter
 from gathernomics.filters.govexp import GovernmentExpenditureFilter
 from gathernomics.filters.capital import CaptialFilter
+from gathernomics.filters.ieport import ImportExportFilter
