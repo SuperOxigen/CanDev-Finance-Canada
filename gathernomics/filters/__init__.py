5eecf64f68e459d91d7b3bdb2935526ea4ba47d8
SuperOxigen <alex@snowycloud.com>
2018-10-21 10:48:23 -0400

    Finished the consumption filters.

--- gathernomics/filters/__init__.py
+++ gathernomics/filters/__init__.py
@@ -4,7 +4,10 @@ Copyright (c) 2018 Alex Dale
 See LICENSE for information.
 """
 
-from gathernomics.filters.consumption import ConsumptionFilter
+from gathernomics.filters.consumption import (
+    ConsumptionFilter, ConsumptionTaxFilter, ConsumptionEmploymentRateFilter,
+    ConsumptionWagesFilter, ConsumptionDisposableIncomeFilter,
+    ConsumptionCreditFilter)
 from gathernomics.filters.gdp import GDPFilter
 from gathernomics.filters.govexp import GovernmentExpenditureFilter
 from gathernomics.filters.capital import CaptialFilter
